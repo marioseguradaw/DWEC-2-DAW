116a584d1aac0ae381e8d675947afd732dead315
Mario Segura Abad <marsegaba@alu.edu.gva.es>
2026-03-25 17:49:48 +0100

    Nueva carpeta añadida denominada 'Teoria' en la cual iré copiando absolutamente todo el código que vea y pueda de los apartados correspondientes de la teoría del tema 2

---
+++ Repaso DWEC T1 - T5/U02/Teoria/DWEC_U02_A02_01.js
@@ -0,0 +1,34 @@
+// Autor: Mario Segura Abad
+// Fecha: 24/03/2026
+
+
+// 2.1. Parámetros y variables locales
+function pintar(color="negro") {
+    console.log(`Pintando con color ${color}`);
+}
+
+pintar(); // Pintando con color negro
+pintar("azul"); // Pintando con color azul
+
+// 2.2. Funciones como valor
+function sumar(a, b) {
+    return a + b;
+}
+let suma1 = sumar(4,5); // suma1 = 9
+let funcSuma = sumar; // funcSuma almacena la funcion, no el resultadao de su ejecución
+funcSuma(3,4); // 7
+let suma2 = funcSuma(3,6); // suma2 = 9
+
+/* Pasadas como parámetro a otras funciones */
+function sumar(a, b) {
+    return a + b;
+}
+function restar(a, b) {
+    return a - b;
+}
+function operar(operacion, a, b) {
+    return operacion(a,b);
+}
+
+let v1 = operar(sumar, 4, 5); // v1 = 9
+let v2 = operar(restar, 4, 5); // v2 = -1
---
+++ Repaso DWEC T1 - T5/U02/Teoria/DWEC_U02_A02_02.js
@@ -0,0 +1,17 @@
+// Autor: Mario Segura Abad
+// Fecha: 24/03/2026
+
+
+// 2.3. Funciones anónimas
+let sumar = function(a, b) {
+    return a + b;
+}
+
+let v1 = sumar(4,5); // 9
+
+/* Función con nombre */
+let sumar2 = function sumar(a, b) {
+    return a + b;
+}
+
+let v2 = sumar(4, 5); // 9
