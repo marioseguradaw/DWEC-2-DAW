956dbf11a6eb329047e853fd0a818f00667b455d
Mario Segura Abad <marsegaba@alu.edu.gva.es>
2026-05-07 17:13:02 +0200

    Modificación del archivo gestionPresupuesto.js en la carpeta 'U02 - JavaScript' a falta de terminar el ejercicio del todo

--- Repaso DWEC T1 - T5/U02 - JavaScript/Actividades/DWEC_U02_A03_z_CPE_E/js/gestionPresupuesto.js
+++ Repaso DWEC T1 - T5/U02 - JavaScript/Actividades/DWEC_U02_A03_z_CPE_E/js/gestionPresupuesto.js
@@ -1,19 +1,47 @@
-// TODO: Variables globales
+// Autor: Mario Segura Abad
+// Fecha: 05/05/2026
 
+let presupuesto = 0;
 
 // TODO: Funciones adicionales
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(nuevoPresupuesto) {    
+    if (nuevoPresupuesto >= 0) {
+        presupuesto = nuevoPresupuesto;
+        return presupuesto;
+    } else {
+        console.log("Error, número no válido. El número debe ser positivo.");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    // Atributos
+    this.descripcion = descripcion;
+    this.valor = (typeof(valor) === "number" && valor >= 0) ? valor : 0;
+
+    // Métodos
+    this.mostrarGasto = function () {
+        return `Gasto correspondiente a ${descripcion} con valor ${valor} €`;
+    };
+
+    this.actualizarValor = function (nuevoValor) {
+        this.valor = (typeof(nuevoValor) === "number" && nuevoValor >= 0) ? nuevoValor : this.valor; 
+    }
+
+    this.actualizarDescripcion = function (nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+
+    /*if (valor >= 0) {
+        this.valor = valor;
+    } else {
+        this.valor = 0;
+    } */
 }
 
 // Exportación de funciones
