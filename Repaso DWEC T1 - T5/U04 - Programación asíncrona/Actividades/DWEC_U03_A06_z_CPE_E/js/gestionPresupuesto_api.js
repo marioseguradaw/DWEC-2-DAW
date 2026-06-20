45ca1d5ed5f2fc24f4069f612b667648fe76b55d
Mario Segura Abad <marsegaba@alu.edu.gva.es>
2026-06-20 13:39:34 +0200

    Ejercicio DWEC_U04_A04_CPE a mi manera terminado dentro de la carpeta 'U04 - Programación asíncrona'. Nueva carpeta creada denominada EXAMEN DWEC_U04

--- Repaso DWEC T1 - T5/U04 - Programación asíncrona/Actividades/DWEC_U03_A06_z_CPE_E/js/gestionPresupuesto_api.js
+++ Repaso DWEC T1 - T5/U04 - Programación asíncrona/Actividades/DWEC_U03_A06_z_CPE_E/js/gestionPresupuesto_api.js
@@ -44,18 +44,141 @@ function mostrarListadoGastos() {
     listadoDiv.innerHTML = "";
 
     gastos.forEach(gasto => {
+        console.log(gasto);
         const gastoDiv = document.createElement("div");
 
         gastoDiv.textContent =
             `${gasto.descripcion} - ${gasto.valor} € - ${new Date(gasto.fecha).toLocaleDateString()}`;
 
         listadoDiv.appendChild(gastoDiv);
+
+        const botonEditar = document.createElement("button");
+        botonEditar.textContent = "Editar";
+
+        botonEditar.addEventListener("click", event => {
+            const nuevaDescripcion = prompt("Nueva descripción:", gasto.descripcion);
+            const nuevoValor = parseFloat(prompt("Nuevo valor:", gasto.valor));
+
+            actualizarGasto(gasto.id, nuevaDescripcion, nuevoValor, gasto.fecha);
+        });
+        gastoDiv.appendChild(botonEditar);
+
+        const botonBorrar = document.createElement("button");
+        botonBorrar.textContent = "Borrar";
+
+        botonBorrar.addEventListener("click", event => {
+            if (confirm("¿Seguro que deseas borrar este gasto?")) {
+                borrarGasto(gasto.id);
+            }
+        });
+        gastoDiv.appendChild(botonBorrar);
     });
 }
 
 // Inicialización
 document.getElementById("form-usuario");
+
 document.addEventListener("submit", event => {
     event.preventDefault();
     cargarGastos();
 });
+
+// Crea el formulario para añadir nuevos gastos
+function crearFormularioGastos() {
+    // console.log("ENTRANDO EN crearFormularioGastos");
+    const contenedor = document.getElementById("form-gasto");
+
+    const formulario = document.createElement("form");
+    formulario.id = "nuevo-gasto";
+
+    const descripcion = document.createElement("input");
+    descripcion.placeholder = "Descripción";
+    descripcion.required = true;
+
+    const valor = document.createElement("input");
+    valor.placeholder = "Valor";
+    valor.type = "number";
+
+    const fecha = document.createElement("input");
+    fecha.type = "date";
+
+    const boton = document.createElement("button");
+    boton.textContent = "Añadir gasto";
+    boton.type = "submit";
+
+    formulario.append(descripcion, valor, fecha, boton);
+
+    contenedor.appendChild(formulario);
+
+    // Registrar el submit
+    formulario.addEventListener("submit", event => {
+        event.preventDefault();
+
+        const gasto = {
+            descripcion:
+                descripcion.value,
+
+            valor:
+                parseFloat(valor.value),
+        
+            fecha:
+                Date.parse(fecha.value)
+        };
+
+        guardarGasto(gasto);
+    });
+}
+
+function guardarGasto(gasto) {
+    fetch(`http://localhost:3000/${usuarioActual}`,
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(gasto)
+        }
+    )
+    .then(() => {
+        cargarGastos();
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+function actualizarGasto(gastoId, descripcion, valor, fecha) {
+    const gastoActualizado = {
+        descripcion,
+        valor,
+        fecha
+    };
+
+    fetch(`http://localhost:3000/${usuarioActual}/${gastoId}`,
+        {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(gastoActualizado)
+        }
+    )
+    .then(() => {
+        cargarGastos();
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+function borrarGasto(gastoId) {
+    fetch(`http://localhost:3000/${usuarioActual}/${gastoId}`,
+        {
+            method: "DELETE"
+        }
+    )
+    .then(() => {
+        cargarGastos();
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+crearFormularioGastos();
